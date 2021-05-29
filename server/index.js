cf7eb138db509ac2f4fd736126f74ca5ef4eaaa5
Muhammad Haseeb <haseeb.lums@gmail.com>
2021-05-29 18:17:31 +0000

    updated routes

--- server/index.js
+++ server/index.js
@@ -2,20 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const laodEnv = require('dotenv').config({ path: '.env' });
+
 const { MongoClient } = require("mongodb");
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.xbcxm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useUnifiedTopology: true
 });
 const testMongoDBConnection = require('./utils');
-
-// Test mongodb connection
 testMongoDBConnection(client);
 
-// Server Static assets
+const tiles = require('./routes/tiles');
+const rooms = require('./routes/rooms');
+const decor = require('./routes/decor');
+
 app.use('/', express.static('../client/build'))
 
-// Start Server
+app.use('/tiles', tiles);
+app.use('/rooms', rooms);
+app.use('/decor', decor);
+
 app.listen(PORT, () => {
     console.log(`Shaheen is listening at http://localhost:${PORT}`)
 });
