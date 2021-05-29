d9b997c8ceec1cfed49447801f065c790806ecb9
Muhammad Haseeb <haseeb.lums@gmail.com>
2021-05-29 18:05:10 +0000

    added api.js

---
+++ server/routes/api.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const router = express.Router();
+
+router.get('/tiles', (req, res, next) => {
+
+});
+
+router.get('/rooms', (req, res, next) => {
+
+});
+
+router.post('/decor', (req, res, next) => {
+
+});
+
+module.exports = router;
