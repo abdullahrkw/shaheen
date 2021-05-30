83291741f91a8b55c1caeeadd35003692defa3ff
Muhammad Haseeb <haseeb.lums@gmail.com>
2021-05-30 13:53:09 +0000

    dynamically loading tiles and rooms from store

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,16 +1,19 @@
 const express = require ('express');
+const store = require("./store");
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    let rooms = [];
+    store.rooms.forEach((ele, ind) => {
+        rooms.push({
+            "id": String(ind + 1),
+            "url": "/assets/rooms/" + ele,
+            "maps": []
+        });
+    });
+
     res.json({
-        "rooms" : [
-            {
-                "id": "1",
-                "url": "/assets/rooms/1.png", 
-                "maps": [
-                ]
-            }
-        ]
+        "rooms" : rooms
     });
 });
 
--- server/routes/tiles.js
+++ server/routes/tiles.js
@@ -1,12 +1,19 @@
 const express = require ('express');
+const store = require("./store");
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+
+    let tiles = [];
+    store.tiles.forEach((ele, ind) => {
+        tiles.push({
+            "id": String(ind + 1),
+            "url": "/assets/tiles/" + ele
+        });
+    });
+
     res.json({
-        "tiles" : [
-            {"id": "1", "url": "/assets/tiles/1.png"},
-            {"id": "2", "url": "/assets/tiles/2.png"},
-        ]
+        "tiles" : tiles
     });
 });
 
