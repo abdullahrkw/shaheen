95dd20049b1a1cc946934a3aaaeafbd41e77ffe0
root <abdullah.nawazrkw@gmail.com>
2021-05-31 20:44:07 +0000

    mention root dir for static files (required by heroku)

--- index.js
+++ index.js
@@ -19,14 +19,13 @@ const tiles = require('./routes/tiles');
 const rooms = require('./routes/rooms');
 const decor = require('./routes/decor');
 
-app.use('/', express.static(`${process.env.PWD}/client/build`))
+app.use(express.static(`${process.env.PWD}/client/build`))
 
 app.use('/tiles', tiles);
 app.use('/rooms', rooms);
 app.use('/decor', decor);
 app.use('/assets', express.static(`${process.env.PWD}/assets`));
 
-console.log(process.env)
 app.listen(PORT, () => {
     console.log(`Shaheen is listening at http://localhost:${PORT}`)
 });
