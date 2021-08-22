9b5088afd29b942b893ceb9b8b07497117076ad8
abdullahrkw <abdullah.nawazrkw@gmail.com>
2021-08-22 08:21:34 +0000

    fixing loader

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -30,7 +30,7 @@ class Room extends React.Component {
                 <svg viewBox="0 0 1800 1012"
                     preserveAspectRatio="none"
                     xmlns="http://www.w3.org/2000/svg">
-                    <image href={this.state.room} height="100%" width="100%" />
+                    <image href={this.state.room} height="100%" width="100%" opacity={isLoading?"0.2":"1"} onLoad={this.props.stopRoomLoading}/>
                     <polygon onClick={this.props.makeTilesVisible} points="0,1001 0,829 456,573 461,594 789,594 937,613 1089,619 1142,770 1329,775 1336,705 1800,1010 0,1010" fill="yellow" opacity="0" />
                 </svg>
 
