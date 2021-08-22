b53abe6a5bf8bc645272892b255ea2b0b06c12ae
abdullahrkw <abdullah.nawazrkw@gmail.com>
2021-08-22 08:28:16 +0000

    fixing loader

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -17,11 +17,7 @@ class Room extends React.Component {
     }
 
     render() {
-        let isLoading = this.props.isLoading;
-        let roomStyle = {};
-        if (isLoading) {
-            roomStyle = { "opacity": 0.2 };
-        }
+        let isLoading = this.props.isLoading;        
         return (
             <div className="Room">
                 <div className="loader">
@@ -31,7 +27,7 @@ class Room extends React.Component {
                     preserveAspectRatio="none"
                     xmlns="http://www.w3.org/2000/svg">
                     <image href={this.state.room} height="100%" width="100%" opacity={isLoading?"0.2":"1"} onLoad={this.props.stopRoomLoading}/>
-                    <polygon onClick={this.props.makeTilesVisible} points="0,1001 0,829 456,573 461,594 789,594 937,613 1089,619 1142,770 1329,775 1336,705 1800,1010 0,1010" fill="yellow" opacity="0" />
+                    <polygon display={isLoading?"none":"block"} onClick={this.props.makeTilesVisible} points="0,1001 0,829 456,573 461,594 789,594 937,613 1089,619 1142,770 1329,775 1336,705 1800,1010 0,1010" fill="yellow" opacity="0" />
                 </svg>
 
             </div>
